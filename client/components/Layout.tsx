462e0063e5175f9cb7421e398ced048ed9f71d2a
James Crabtree <143312545+james-crabtree@users.noreply.github.com>
2023-11-10 11:43:08 +1300

    Intergrated getJoke with Handleclick

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,19 +1,29 @@
 import { Outlet } from 'react-router-dom'
 import { useState } from 'react'
+import { getJoke } from '../apiClient'
 
 function Dadbtn() {
   const [joke, setJoke] = useState('default joke')
 
   const handleClick = () => {
-    setJoke('new joke')
+    getJoke()
+      .then((result) => {
+        console.log(result)
+        setJoke(result.text)
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
   }
 
   return (
     <>
-      <button onClick={handleClick}>
-        <img src="images/dadbtn.png" alt="dadbodsexyasf" />
-      </button>
-      <p>{joke}</p>
+      <div className="joke-container">
+        <button onClick={handleClick}>
+          <img src="images/dadbtn.png" alt="dadbodsexyasf" />
+        </button>
+        <p>{joke}</p>
+      </div>
     </>
   )
 }
@@ -21,7 +31,7 @@ export default function Layout() {
   return (
     <>
       <header>
-        <h1>Bad Woke Dad Jokes</h1>
+        <h1>Bad Woke Dad Joke</h1>
       </header>
       <main>
         <br />
