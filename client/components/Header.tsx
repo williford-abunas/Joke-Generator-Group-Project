abb5b7d22b91e72b26fe8522225432275beaf33e
Williford Abunas <142576037+williford-abunas@users.noreply.github.com>
2023-11-10 14:32:02 +1300

    header style

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -21,17 +21,40 @@ export default function WellyWeather() {
   }, [])
 
   return (
-    <div style={{ display: 'flex' }}>
-      <p style={{ color: 'red' }}>
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '40px',
+        backgroundColor: 'slategray',
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        paddingBottom: '10px',
+        borderBottom: '3px solid black',
+      }}
+    >
+      <p style={{ color: 'white', fontSize: '20px', paddingLeft: '10px' }}>
         {weather?.name}, {weather?.sys.country} ||
       </p>
-      <p>
-        <span style={{ color: 'blue' }}> Temp:</span> {weather?.main.temp} ||
+      <p style={{ fontSize: '20px', color: 'salmon' }}>
+        <span style={{ color: 'white' }}>
+          <span style={{ color: 'lightslategray' }}>_</span>Temp:
+        </span>
+        {weather?.main.temp} &deg;C
       </p>
-      <p>
-        <span style={{ color: 'blue' }}> Weather:</span>{' '}
-        {weather?.weather[0].description}{' '}
+      <p style={{ fontSize: '20px', color: 'salmon' }}>
+        <span style={{ color: 'white' }}>
+          <span style={{ color: 'lightslategray' }}>_</span>|| Weather:
+        </span>
+        {weather?.weather[0].description}
       </p>
+      <img
+        src="https://openweathermap.org/img/w/04d.png"
+        alt="Weather Icon"
+        style={{ height: '30px', width: '30px', position: 'relative' }}
+      />
+      <p style={{ color: 'white', fontSize: '20px', paddingLeft: '300px' }}>Why did the weather go to school? <span style={{ color: 'salmon' }}>To get a little brighter!</span></p>
     </div>
   )
 }
